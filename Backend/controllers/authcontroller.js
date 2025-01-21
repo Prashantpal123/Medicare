b130fda1464714a2dcfbd250d9d8a0f0f68564f2
Prashant Pal <142970947+Prashantpal123@users.noreply.github.com>
2025-01-21 09:18:28 +0530

    feat/backend-setup

---
+++ Backend/controllers/authcontroller.js
@@ -0,0 +1,22 @@
+export const register = async(req, res)=>{
+   
+    try {
+        
+    } catch (error) {
+        
+    }
+ }
+
+
+ ///Login////
+
+
+
+ export const login = async(req, res)=>{
+   
+    try {
+        
+    } catch (error) {
+        
+    }
+ }
---
+++ Backend/index.js
@@ -0,0 +1,49 @@
+import express, { json } from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import  dotenv from "dotenv";
+
+dotenv.config();
+const app=express()    
+const port =process.env.Port || 8000
+
+
+const corsOption=
+{
+    origin:true,
+}
+
+
+app.get('/',(req,res)=>
+{
+    res.send("Api is working")
+
+});
+
+mongoose.set("strictQuery",false)
+const connectDB= async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL, {
+        
+            
+           
+        })
+        console.log("mongoDB is connected Sucessfully")    }
+
+    catch(err)
+    {
+        console.log("mongoDb connection failed")
+    }
+}
+
+
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOption));
+app.listen(port,()=>
+{ connectDB();
+    console.log("Server is running on port "+port);
+})
