8d808e5019c7a2d890059a029cc1931f17562016
Prashant Pal <142970947+Prashantpal123@users.noreply.github.com>
2025-03-07 09:08:09 +0530

    deploy/eror/fix

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
-import Routers from '../routes/routers'
+import Routers from '../routes/Router'
 
 const Layout = () => {
   return (
